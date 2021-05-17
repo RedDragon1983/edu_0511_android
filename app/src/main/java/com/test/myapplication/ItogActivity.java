19e2f3b07dc08b9a570037b72961289429caa316
Николай <82583556+RedDragon1983@users.noreply.github.com>
2021-05-17 12:26:32 +0300

    Update ItogActivity.java

--- app/src/main/java/com/test/myapplication/ItogActivity.java
+++ app/src/main/java/com/test/myapplication/ItogActivity.java
@@ -1,12 +1,16 @@
 package com.test.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ItogActivity extends AppCompatActivity {
 
     private TextView answerTextView;
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,13 @@ public class ItogActivity extends AppCompatActivity {
 
         answerTextView = findViewById(R.id.answerTextView);
         answerTextView.setText(getIntent().getStringExtra("answer2"));
+        start = findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItogActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
